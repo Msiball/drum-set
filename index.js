31e46da820cac086302f15595d13ce36298a54e0
Manpreet Singh <manpreet0361.ms@gmail.com>
2024-06-11 17:22:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
 // for all drum buttons to work when clicked
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
 
-  function handleClick() {
-    alert("Button clicked!");
-  }
+    switch (buttonInnerHTML) {
+      case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+      default:
+        console.log(buttonInnerHTML + " button not found");
+    }
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "w") {
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+    } else if (event.key === "a") {
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+    } else if (event.key === "s") {
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+    } else if (event.key === "d") {
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+    } else if (event.key === "j") {
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+    } else if (event.key === "k") {
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+    } else if (event.key === "l") {
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+    }
+  });
 }
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//    audio.play();
